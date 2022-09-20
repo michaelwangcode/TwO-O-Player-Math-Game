828af1f2f0695ec5c4b77dc873eeea88d580b413
michaelwangcode <michaelwangxv@gmail.com>
2022-09-20 03:52:11 -0400

    Completed game

--- Question.rb
+++ Question.rb
@@ -1,18 +1,21 @@
 # This class creates question for the math game
 class Question
 
+  # Initialize values for question
   def initialize
     @num1 = rand(1..20)
     @num2 = rand(1..20)
     @sum = String(@num1 + @num2)
   end
 
+  # Print a question
   def print_question(id)
     puts "Player #{id}: What does #{@num1} plus #{@num2} equal?"
   end
 
+  # Check the answer for a question and return true or false
   def check_answer(answer)
-    return @sum == answer
+    @sum == answer
   end
 
 end
